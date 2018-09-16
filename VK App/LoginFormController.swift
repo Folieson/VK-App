007eb3868e3fc79d1d916b7f45890f3938056576
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2018-09-16 14:45:16 +0500

    1. Создано приложение; 2. Добавлена форма для входа; 3. Форма адаптирована для телефонов в альбомной ориентации, планшетов.

---
+++ VK App/LoginFormController.swift
@@ -0,0 +1,103 @@
+//
+//  LoginFormController.swift
+//  VK App
+//
+//  Created by Андрей Понамарчук on 16.09.2018.
+//  Copyright © 2018 Андрей Понамарчук. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var loginInput: UITextField!
+    @IBOutlet var passwordInput: UITextField!
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+        
+        // Получаем текст логина
+        let login = loginInput.text!
+        
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+        
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        // Присваиваем его UIScrollView
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification:Notification) {
+        
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    // Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на два уведомления: одно приходит при появлении кдавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        // Второе - когда оно пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    // Отписываемся от уведомлений
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    // Метод скрытия клавиатуры
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
